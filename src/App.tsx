795561f24c8dd9b895f9d791c883556067f18aa4
mnxmnz <minzidev@gmail.com>
2025-03-22 09:29:47 +0900

    feat: 필터 가능한 제품 테이블 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import FilterableProductTable from './components/FilterableProductTable';
+import { PRODUCTS } from './data/products';
+
 function App() {
   return (
     <>
       <h1>Product Table</h1>
+      <FilterableProductTable products={PRODUCTS} />
     </>
   );
 }
